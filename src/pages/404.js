3695b4b0d0fb937c7d725ad183da1316ced633b8
Harixafeo <airmailtohari@gmail.com>
2021-04-17 01:04:12 +0530

    mark up and transformer plugins done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import Layout from './components/Layout';
+import React from 'react'
+import Layout from '../components/Layout'
 
-export default function Error() {
+const NotFound = () => {
   return (
     <Layout>
-    <div>
-        <h1>Oops, this page doesnt exist</h1>
-    </div>
+      <div>
+        <h2>404</h2>
+        <p>Sorry, that page doesn't exist (yet)!</p>
+      </div>
     </Layout>
   );
 }
+ 
+export default NotFound
